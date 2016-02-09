1d1cb35d93d7dc58a5766786d031000d1e0efb2c
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-02-09 09:23:50 +0000

    more testing.

--- tests/test_sqs_processor.py
+++ tests/test_sqs_processor.py
@@ -17,11 +17,11 @@ def test_process_sms_content_message(mocker,
                                      mock_alpha_send_sms):
     from tests.conftest import populate_queue_with_sms_content_msg
     populate_queue_with_sms_content_msg(mocker, delivery_config)
-    # process_all_queues(delivery_config, delivery_config['NOTIFICATION_QUEUE_PREFIX'])
-    # msg = populate_queue_with_sms_content_msg.receive_messages()[0]
-    # content = decrypt_content(delivery_config, msg.body)
-    # mock_alpha_send_sms.assert_called_with(content['to'], content['content'])
-    # assert msg.delete.call_count == 1
+    process_all_queues(delivery_config, delivery_config['NOTIFICATION_QUEUE_PREFIX'])
+    msg = populate_queue_with_sms_content_msg.receive_messages()[0]
+    content = decrypt_content(delivery_config, msg.body)
+    mock_alpha_send_sms.assert_called_with(content['to'], content['content'])
+    assert msg.delete.call_count == 1
 
 
 # @moto.mock_sqs
